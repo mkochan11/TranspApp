dba43f3125b8b4f985cbcdf1c23d9871cdedfe17
mkochan11 <kochanowski.michal.11@gmail.com>
2023-10-19 00:38:23 +0200

    Initialize Authentication

---
+++ Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity
+{
+    internal class AppIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+        : base(options)
+        {
+        }
+
+
+    }
+}
